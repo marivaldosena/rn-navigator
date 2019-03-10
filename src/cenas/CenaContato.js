f46efc41c0e041f36a892e1e5c38bff0d4fe523c
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-10 16:14:51 -0300

    Incluir imagem da cena e informações de contato

--- src/cenas/CenaContato.js
+++ src/cenas/CenaContato.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  StatusBar,
+  StyleSheet,
+  Image
 } from 'react-native';
+import BarraNavegacao from '../components/BarraNavegacao';
+import detalheContatoImage from '../images/detalhe_contato.png';
 
-const CenaContato = () => (
-  <View>
-    <Text>Cena Contato</Text>
-  </View>
-);
+const CenaContato = ({ navigator }) => {
+  const {
+    cabecalho,
+    titulo,
+    detalhes,
+    texto
+  } = estilos;
+
+  return (
+    <View>
+      <StatusBar
+        backgroundColor='#ccc'
+      />
+      <BarraNavegacao voltar navigator={navigator} />
+      
+      <View style={cabecalho}>
+        <Image source={detalheContatoImage} />
+        <Text style={titulo}>Contatos</Text>
+      </View>
+
+      <View style={detalhes}>
+        <Text style={texto}>Tel: (11) 1234-1234</Text>
+        <Text style={texto}>Cel: (11) 98765-4321</Text>
+        <Text style={texto}>Email: contato@email.com</Text>
+      </View>
+    </View>
+
+  );
+};
+
+const estilos = StyleSheet.create({
+  cabecalho: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  titulo: {
+    fontSize: 30,
+    color: '#61bd8c',
+    marginLeft: 10,
+    marginTop: 25,
+  },
+  detalhes: {
+    padding: 20,
+    marginTop: 20,
+  },
+  texto: {
+    fontSize: 18,
+    paddingLeft: 10
+  },
+});
 
 export default CenaContato;
